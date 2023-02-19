43b73ee82d049a41bf12e9032e9ae87c9b00a9e4
Samuel Gallagher <segallagher@alaska.edu>
2023-02-18 22:19:50 -0900

    credentials.json now has proper objects, passwords cannot contain username

--- app.js
+++ app.js
@@ -23,17 +23,20 @@ app.get('/', (req, res) =>{
 
 app.post('/login', (req,res)=>{
     // read file for now and save username and password
-    fs.readFile("credentials.json", 'utf8', (err,data)=>{
-        const obj = JSON.parse(data);
-        var remainingAttempts = req.body.remainingAttempts;
-        if(obj[req.body.username] != undefined){
-            console.log("youre in");
-            res.redirect('/');
-        }else{
-            remainingAttempts--;
-            res.render('nfLogin',{remainingAttempts: remainingAttempts});
-        }
-    });
+    const data = fs.readFileSync("credentials.json");
+    const json = JSON.parse(data);
+    const currUser = json.find(obj => obj['username'] === req.body.username);
+
+    var remainingAttempts = req.body.remainingAttempts;
+    
+    if(currUser){
+        console.log("Logged in");
+        res.redirect('/');
+    }else{
+        remainingAttempts--;
+        console.log("Username or password incorrect\n " + remainingAttempts + " attempts remaining");
+        res.render('nfLogin',{remainingAttempts: remainingAttempts});
+    }
 });
 
 app.get('/signup', (req, res) =>{
@@ -42,17 +45,28 @@ app.get('/signup', (req, res) =>{
 
 app.post('/createUser', (req,res)=>{
     // write to saved file a new user
-    console.log("signupTime");
-    fs.readFile("credentials.json", 'utf8', (err,data)=>{
-        const obj = JSON.parse(data);
-        if(obj[req.body.username] != undefined){
-            console.log("username taken");
-            res.render('signup',{response: "Username Taken"});
-        }else{
-            console.log("Account Created");
-            res.render('signup',{response: "Account Created"});
-        }
-    });
+    var data = fs.readFileSync("credentials.json");
+    var json = JSON.parse(data);
+    const currUser = json.find(obj => obj['username'].toUpperCase() === req.body.username.toUpperCase());
+
+    if(currUser){
+        console.log("username taken");
+        res.render('signup',{response: "Username Taken"});
+    }else if(req.body.password.includes(req.body.username)){
+        console.log("Password cannot contain username");
+        res.render('signup',{response: "Username Taken"});
+    }else{
+        var obj = {
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email
+        };
+        json.push(obj);
+        const newObj = JSON.stringify(json);
+        fs.writeFileSync("credentials.json",newObj,"utf-8");
+        console.log("Account Created");
+        res.render('signup',{response: "Account Created"});
+    }
 });
 
 app.get('/sign-up',(req,res) =>{
